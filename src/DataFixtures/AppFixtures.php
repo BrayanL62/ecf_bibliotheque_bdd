55bbfe3116f42d207023507d2554e51042955b90
BrayanL <brayan.loucher@outlook.com>
2021-06-06 09:05:35 +0200

    F Injecting test data into the following entities : Admin / Borrowers / Kinds
    [php] [data]

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Kind;
+use App\Entity\Borrower;
+use App\Entity\Book;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
@@ -14,11 +17,113 @@ class AppFixtures extends Fixture
     private $encoder;
     private $faker;
 
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+        $this->faker = FakerFactory::create('fr_FR');
+    }
+
+    public static function getGroups(): array
+    {
+        // Cette fixture fait partie du groupe "test".
+        // Cela permet de cibler seulement certains fixtures
+        // quand on exécute la commande doctrine:fixtures:load.
+        // Pour que la méthode statique getGroups() soit prise
+        // en compte, il faut que la classe implémente
+        // l'interface FixtureGroupInterface.
+        return ['test'];
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //On définit le nombre d'objets qu'il va falloir créer
+        $booksCount = 1000;
+
+        // On appelle les fonctions qui vont créer les objets dans la BDD
+        $this->loadAdmin($manager);
+        $borrowers = $this->loadBorrowers($manager);
+        $kinds = $this->loadKinds($manager);
+        // $books = $this->loadBooks($manager, $booksCount);
 
         $manager->flush();
     }
+
+    public function loadAdmin(ObjectManager $manager)
+    {
+        // création d'un user avec des données constantes
+        // ici il s'agit du compte admin
+        $user = new User();
+        $user->setEmail('admin@example.com');
+        // hashage du mot de passe
+        $password = $this->encoder->encodePassword($user, '123');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+    }
+
+    public function loadBorrowers(ObjectManager $manager)
+    {
+        $borrowers = [];
+
+        $user = new User();
+        $user->setEmail('bar.bar@example.com');
+        // hashage du mot de passe
+        $password = $this->encoder->encodePassword($user, '123');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_BORROWER']);
+
+        $manager->persist($user);
+
+        // Création d'un borrower avec des données constantes
+        $borrower = new Borrower();
+        $borrower->setLastname('bar');
+        $borrower->setFirstname('bar');
+        $borrower->setPhoneNumber('123456789');
+        $borrower->setActive(false);
+        $borrower->setCreationDate(\DateTime::createFromFormat('Y-m-d H:i:s', '2020-01-01 00:00:00'));
+        $borrower->setUser($user);
+
+        $manager->persist($borrower);
+
+        $borrowers[] = $borrower;
+
+    }
+
+    public function loadKinds(ObjectManager $manager)
+    {
+        // On déclare un tableau qui servira à stocker les différents kinds 
+        $kinds = [];
+
+        $kind = new Kind();
+        $kind->setName('poésie');
+        
+        $manager->persist($kind);
+
+        $kinds[] = $kind;
+    }
+
+    // public function loadBooks(ObjectManager $manager, int $count)
+    // {
+    //     // Création d'un tableau qui contiendra les books qu'on va créer.
+    //     // La fonction va pouvoir renvoyer ce tableau pour que d'autres fonctions
+    //     // de création d'objets puissent utiliser les books.
+    //     $books = [];
+
+    //     // Création d'un book avec des données constantes.
+    //     $book = new Book();
+    //     $book->setTitle('Lorem Ipsum Dolor Sit Amet');
+    //     $book->setPublicationYear(2010);
+    //     $book->setNumberPages(100);
+    //     $book->setIsbnCode('9785786930024');
+    //     $book->setAuthor();
+
+    //     $manager->persist($book);
+
+    //     // On ajoute le premier book que l'on vient de créer
+    //     $books[] = $book;
+
+
+    // }
+
 }
