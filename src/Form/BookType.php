43c08c0defd3bae57fb7bf218ded6ef59577bfbf
BrayanL <brayan.loucher@outlook.com>
2021-07-02 22:11:33 +0200

    F setup CRUD for Book entity
    Same as User or Author, we can CRUD all entities, this time, Book index is done
    
    [Book]

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -3,8 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Book;
+use App\Entity\Author;
+use App\Entity\Kind;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BookType extends AbstractType
@@ -16,9 +21,25 @@ class BookType extends AbstractType
             ->add('publication_year')
             ->add('number_pages')
             ->add('isbn_code')
-            ->add('author')
-            ->add('kind')
-            ->add('kinds')
+            ->add('author', EntityType::class, [
+                // Looks for choices from this entity
+                'class' => Author::class,
+
+                'choice_label' => function(Author $author){
+                    return "{$author->getLastname()} {$author->getFirstname()}";
+                },
+                
+            ])
+            ->add('kind', EntityType::class, [
+                'class' => Kind::class,
+
+                'choice_label' => function(Kind $kind){
+                    return "{$kind->getName()}";
+                },
+                'multiple' => true,
+                'expanded' => true
+            ])
+            // ->add('kinds')
         ;
     }
 
